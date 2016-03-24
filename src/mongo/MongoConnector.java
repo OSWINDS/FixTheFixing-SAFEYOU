34dd61af7d9550b3e8aa8f050651f63eec446e81
Sofia Yfantidou <Sofia Yfantidou>
2016-03-24 16:41:59 +0200

    Initial commit

---
+++ src/mongo/MongoConnector.java
@@ -0,0 +1,42 @@
+package mongo;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+/**
+ * Created by sifantid on 24/3/2016.
+ */
+public class MongoConnector {
+
+    public static final String _TWEET_JSON_  = "tweet";
+    public static final String _TWEET_TREND_ = "trend";
+    public static final String _TWEET_PARSED_STRING_ = "parsedString";
+    public static final String _TWEET_EMOTION_SCORES_ = "emScores";
+
+    private MongoDatabase _db;
+    /**
+     * Connects to the given server:port to the database vakali and accesses the tables prefix_*
+     * @param host The host name of the server
+     * @param port The port of the server
+     */
+    public MongoConnector(String host, int port) {
+        MongoClient mongoClient = new MongoClient(host, port);
+        _db = mongoClient.getDatabase("fixthefixing");
+    }
+
+    public void addTweet(String trend, String json) {
+        MongoCollection<Document> coll = _db.getCollection("tweets");
+
+        Document doc = new Document(_TWEET_TREND_, trend)
+                .append(_TWEET_PARSED_STRING_, null)
+                .append(_TWEET_JSON_, Document.parse(json));
+
+        coll.insertOne(doc);
+    }
+
+    public static void main(String[] args) {
+        MongoConnector mongoConnector = new MongoConnector("localhost", 27017);
+    }
+}
