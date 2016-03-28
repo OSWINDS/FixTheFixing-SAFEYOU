5f803c6a47918d41d8b366752f5aa2474df295b7
Sofia Yfantidou <syfantid@csd.auth.gr>
2016-03-29 00:55:14 +0300

    Added new function

--- src/mongo/MongoConnector.java
+++ src/mongo/MongoConnector.java
@@ -1,21 +1,31 @@
 package mongo;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import javafx.util.Pair;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 /**
  * Custom class that connects to MongoDB and inserts, edits and deletes documents
  */
 public class MongoConnector {
 
+    // Fields for twitter DB
     public static final String _TWEET_JSON_  = "tweet";
-    public static final String _TWEET_TREND_ = "trend";
     public static final String _TWEET_PARSED_STRING_ = "parsedString";
     public static final String _TWEET_EMOTION_SCORES_ = "emScores";
+    // Fields for youtube DB
+    public static final String _COMMENT_JSON_  = "comment";
+    public static final String _COMMENT_PARSED_STRING_ = "parsedString";
+    public static final String _COMMENT_EMOTION_SCORES_ = "emScores";
+
+    private static final String _COLL_INDEX_ = "_id";
 
     private MongoDatabase _db;
+    private MongoDatabase _db_youtube;
     /**
      * Connects to the given server:port to the database fixthefixing
      * @param host The host name of the server
@@ -23,27 +33,79 @@ public class MongoConnector {
      */
     public MongoConnector(String host, int port) {
         MongoClient mongoClient = new MongoClient(host, port);
-        _db = mongoClient.getDatabase("fixthefixing");
+        _db = mongoClient.getDatabase("twitter");
+        _db_youtube = mongoClient.getDatabase("youtube");
     }
 
     /**
      * Inserts tweet to tweets collection
-     * @param trend The trend the tweet is associated to
      * @param json The tweet's JSON
      */
-    public void addTweet(String trend, String json) {
+    public void addTweet(String json) {
         MongoCollection<Document> coll = _db.getCollection("tweets");
 
-        Document doc = new Document(_TWEET_TREND_, trend)
-                .append(_TWEET_PARSED_STRING_, null)
+        Document doc = new Document(_TWEET_PARSED_STRING_, null)
                 .append(_TWEET_JSON_, Document.parse(json));
 
         coll.insertOne(doc);
     }
 
+    /**
+     * Inserts Youtube comments to comments collection
+     * @param json The comment's JSON
+     */
+    public void addYoutubeComment(String json) {
+        MongoCollection<Document> coll = _db_youtube.getCollection("comments");
+
+        Document doc = new Document(_COMMENT_PARSED_STRING_, null)
+                .append(_COMMENT_JSON_, Document.parse(json));
+
+        coll.insertOne(doc);
+    }
+
+    /**
+     * Searches in the data base for the tweet with ID UUID and changes the parsed text field with
+     * the given
+     * @param UUID The UUID of the document that we want to change
+     * @param parsedText the text that we want to put in the field
+     * @return if the text successfully changed
+     */
+    public boolean insertParsedTweet(ObjectId UUID, String parsedText) {
+
+        /* Get db and document */
+        MongoCollection<Document> tweets = _db.getCollection("tweets");
+        FindIterable<Document> iterable = tweets.find(new Document(_COLL_INDEX_, UUID));
+
+        Document document = iterable.first();
+
+        if (document != null) {
+            /* Document exist*/
+            if (document.replace(_TWEET_PARSED_STRING_, null, parsedText)) {
+                tweets.replaceOne(new Document(_COLL_INDEX_, UUID), document).getMatchedCount();
+                //printMongo("Replaced OK -> " + UUID);
+                return true;
+
+            } else {
+                errorMongo("Replace error -> " + UUID + ", text already parsed!");
+                return false;
+            }
+        }
+
+        errorMongo("Document " + UUID + " doesn't exist here!");
+        return false;
+    }
+
+    /**
+     * Centralised management for error displaying
+     * @param str the error message
+     */
+    private void errorMongo(String str) {
+        System.err.println(str);
+    }
+
     public static void main(String[] args) {
         MongoConnector mongoConnector = new MongoConnector("localhost", 27017);
         // Test insertion
-        mongoConnector.addTweet("#Test","{\"phonetype\":\"N95\",\"cat\":\"WP\"}");
+        mongoConnector.addTweet("{\"phonetype\":\"N95\",\"cat\":\"WP\"}");
     }
 }
