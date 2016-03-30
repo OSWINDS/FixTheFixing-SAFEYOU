4df9ff093035551bc4bc30c23b28ff5ffa9ce668
gogopavl <gogopavl@csd.auth.gr>
2016-03-30 13:21:57 +0300

    Added the following packages: main, manager, model Added external libraries in lib/twitter_libs

---
+++ src/main/Exporter.java
@@ -0,0 +1,85 @@
+package main;
+
+/**
+ * Created by gogopavl on 30/3/2016.
+ */
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+import manager.TweetManager;
+import manager.TwitterCriteria;
+import model.Tweet;
+
+public class Exporter {
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+
+    public static void main(String[] args) {
+        if (args == null || args.length == 0) {
+            System.err.println("You must pass some parameters. Use \"-h\" to help.");
+            System.exit(0);
+        }
+
+        if (args.length == 1 && args[0].equals("-h")) {
+            System.out.println("\nTo use this jar, you can pass the folowing attributes:");
+            System.out.println("   username: Username of a specific twitter account (without @)");
+            System.out.println("      since: The lower bound date (yyyy-mm-aa)");
+            System.out.println("      until: The upper bound date (yyyy-mm-aa)");
+            System.out.println("querysearch: A query text to be matched");
+            System.out.println("  maxtweets: The maximum number of tweets to retrieve");
+
+            System.out.println("\nExamples:");
+            System.out.println("# Example 1 - Get tweets by username [@example]");
+            System.out.println("java -jar got.jar username=example maxtweets=1\n");
+
+            System.out.println("# Example 2 - Get tweets by query search [asian handicap]");
+            System.out.println("java -jar got.jar querysearch=\"asian handicap\" maxtweets=1\n");
+
+            System.out.println("# Example 3 - Get tweets by username and bound dates [example, '2015-09-10', '2015-09-12']");
+            System.out.println("java -jar got.jar username=example since=2015-09-10 until=2015-09-12 maxtweets=1");
+        } else {
+            TwitterCriteria criteria = TwitterCriteria.create();
+
+            for (String parameter : args) {
+                String[] parameterSplit = parameter.split("=");
+
+                if (parameterSplit[0].equals("username")) {
+                    criteria.setUsername(parameterSplit[1]);
+                } else if (parameterSplit[0].equals("since")) {
+                    criteria.setSince(parameterSplit[1]);
+                } else if (parameterSplit[0].equals("until")) {
+                    criteria.setUntil(parameterSplit[1]);
+                } else if (parameterSplit[0].equals("querysearch")) {
+                    //Operators: https://dev.twitter.com/rest/public/search
+                    criteria.setQuerySearch(parameterSplit[1]);
+                } else if (parameterSplit[0].equals("maxtweets")) {
+                    criteria.setMaxTweets(Integer.valueOf(parameterSplit[1]));
+                }
+            }
+
+            try {
+                BufferedWriter bw = new BufferedWriter(new FileWriter("output_got.csv"));
+                bw.write("username;date;retweets;favorites;text;geo;mentions;hashtags;id;permalink");
+                bw.newLine();
+
+                System.out.println("Obtaining tweets... \n");
+
+                for (Tweet t : TweetManager.getTweets(criteria)) {
+                    bw.write(String.format("%s;%s;%d;%d;\"%s\";%s;%s;%s;\"%s\";%s", t.getUsername(), sdf.format(t.getDate()), t.getRetweets(), t.getFavorites(), t.getText(), t.getGeo(), t.getMentions(), t.getHashtags(), t.getId(), t.getPermalink()));
+                    bw.newLine();
+                }
+
+                bw.close();
+
+                System.out.println("Done. Output file generated \"output_got.csv\".");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
---
+++ src/manager/TwitterCriteria.java
@@ -0,0 +1,99 @@
+package manager;
+
+/**
+ * Created by gogopavl on 30/3/2016.
+ */
+
+/**
+ * A class to guide how the tweets must be searched on {@link TweetManager}
+ */
+public class TwitterCriteria {
+
+    private String username;
+
+    private String since;
+
+    private String until;
+
+    private String querySearch;
+
+    private int maxTweets;
+
+    private TwitterCriteria() {
+    }
+
+    public static TwitterCriteria create() {
+        return new TwitterCriteria();
+    }
+
+    /**
+     * @param username (without @) Username of a specific twitter account
+     *
+     * @return Current {@link TwitterCriteria}
+     */
+    public TwitterCriteria setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    /**
+     * @param since The lower bound date (yyyy-mm-aa)
+     *
+     * @return Current {@link TwitterCriteria}
+     */
+    public TwitterCriteria setSince(String since) {
+        this.since = since;
+        return this;
+    }
+
+    /**
+     * @param until The upper bound date (yyyy-mm-aa)
+     *
+     * @return Current {@link TwitterCriteria}
+     */
+    public TwitterCriteria setUntil(String until) {
+        this.until = until;
+        return this;
+    }
+
+    /**
+     * @param querySearch A query text to be matched
+     *
+     * @return Current TwitterCriteria
+     */
+    public TwitterCriteria setQuerySearch(String querySearch) {
+        this.querySearch = querySearch;
+        return this;
+    }
+
+    /**
+     * @param maxTweets The maximum number of tweets to retrieve
+     *
+     * @return Current {@link TwitterCriteria}
+     */
+    public TwitterCriteria setMaxTweets(int maxTweets) {
+        this.maxTweets = maxTweets;
+        return this;
+    }
+
+    String getUsername() {
+        return username;
+    }
+
+    String getSince() {
+        return since;
+    }
+
+    String getUntil() {
+        return until;
+    }
+
+    String getQuerySearch() {
+        return querySearch;
+    }
+
+    int getMaxTweets() {
+        return maxTweets;
+    }
+
+}
