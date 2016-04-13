b99a5682fc0097c14f91246f956489a657a4e0aa
Andy Foo <a.m.founta@gmail.com>
2016-04-13 14:34:17 +0300

    create preprocessor class

---
+++ src/combiner/Preprocessor.java
@@ -0,0 +1,42 @@
+package combiner;
+
+/**
+ * Created by Antigoni Founta on 13/4/2016.
+ */
+
+public class Preprocessor {
+
+    //Processor of tweets
+    public static void preprocessTweet(){
+
+    }
+
+    //Processor of youtube comments
+    public static void preprocessComment(){
+
+    }
+
+    public static void removeStopWords(){
+
+    }
+
+    public static void removeMention(){
+
+    }
+
+    public static void removeURL(){
+
+    }
+
+    public static void tokenizer(){
+
+    }
+
+    public static void toLowerCase(){
+
+    }
+
+    public static void removePunctuation(){
+
+    }
+}
